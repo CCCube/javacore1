10b9a2be92323228864051f2a13b25c5729fbc01
CZF <535676766@qq.com>
2015-06-25 16:32:00 +0800

    bank 同步情况下转移金额

--- com.multiprocessing/synch/Bank.java
+++ com.multiprocessing/synch/Bank.java
@@ -10,8 +10,8 @@ import java.util.concurrent.locks.*;
 public class Bank
 {
    private final double[] accounts;
-   private Lock bankLock;
-   private Condition sufficientFunds;
+   private Lock bankLock;  //bankLock 锁对象
+   private Condition sufficientFunds; //sufficientFunds 条件对象
 
    /**
     * Constructs the bank.
@@ -24,7 +24,8 @@ public class Bank
       for (int i = 0; i < accounts.length; i++)
          accounts[i] = initialBalance;
       bankLock = new ReentrantLock();
-      sufficientFunds = bankLock.newCondition();
+      //条件对象，也可以对其上锁，用于上锁条件较多情况下，设置不同条件 
+      sufficientFunds = bankLock.newCondition();  
    }
 
    /**
@@ -35,21 +36,22 @@ public class Bank
     */
    public void transfer(int from, int to, double amount) throws InterruptedException
    {
-      bankLock.lock();
+      bankLock.lock();  //锁对象上锁，当某个线程获得锁，其它线程无法进入try方法块
       try
       {
          while (accounts[from] < amount)
-            sufficientFunds.await();
+            sufficientFunds.await();  //条件对象方法 await() 放弃该条件对象锁且需要等待唤醒 
          System.out.print(Thread.currentThread());
          accounts[from] -= amount;
          System.out.printf(" %10.2f from %d to %d", amount, from, to);
          accounts[to] += amount;
          System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+         //signal()随机选择某个线程唤醒，易死锁  signalAll()唤醒所有等待线程，重新抢占资源
          sufficientFunds.signalAll();
       }
       finally
       {
-         bankLock.unlock();
+         bankLock.unlock();  //注意unlock锁对象
       }
    }
 
@@ -59,7 +61,7 @@ public class Bank
     */
    public double getTotalBalance()
    {
-      bankLock.lock();
+      bankLock.lock();  //同步上锁，返回总金额时不得对bank数组进行修改
       try
       {
          double sum = 0;
